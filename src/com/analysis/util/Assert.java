39bb9bd152377c22f01fb7ef007c081671d51efd
ZhangHao <cloudzhanghi@foxmail.com>
2019-07-17 02:24:10 +0800

    修改工具类

---
+++ src/com/analysis/util/Assert.java
@@ -0,0 +1,19 @@
+package com.analysis.util;
+
+/**
+ * 工具类
+ * @author Cloud
+ *
+ */
+public class Assert {
+
+	/*
+	 * 判断是否为空引用
+	 */
+	public static void notNull(Object obj, String message) {
+		if (obj == null) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+	
+}
