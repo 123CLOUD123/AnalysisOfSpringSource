195c09245db872bc1a5286a738536ef2bebf7237
ZhangHao <cloudzhanghi@foxmail.com>
2019-07-17 02:22:35 +0800

    将Factory类移出上下文类

--- src/com/analysis/context/AppContext.java
+++ src/com/analysis/context/AppContext.java
@@ -1,10 +1,16 @@
 package com.analysis.context;
 
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
 
 import com.analysis.bean.BeanDefinition;
 import com.analysis.bean.BeanReader;
+import com.analysis.postprocessor.BeanFactoryPostProcessor;
+import com.analysis.postprocessor.PostProcessorRegistrationDelegate;
 import com.analysis.support.Registry;
 
 /**
@@ -12,6 +18,8 @@ import com.analysis.support.Registry;
  * @author Cloud
  */
 public class AppContext implements Registry {
+	
+	private final Logger log = Logger.getLogger(getClass().getName());
 
 	/*
 	 * bean容器
@@ -19,22 +27,20 @@ public class AppContext implements Registry {
 	private final BeanFactory factory = new BeanFactory();
 	
 	/*
-	 * 
+	 * bean读取类
 	 */
 	private BeanReader reader;
 	
+	/*
+	 * 启动时间
+	 */
+	private LocalDateTime datetime;
 	
 	/*
-	 * bean容器
-	 * 用来持有bean
+	 * 工厂后置处理器
 	 */
-	private static class BeanFactory {
-		private Map<String, BeanDefinition> beanMap = new ConcurrentHashMap<>(256);
-		
-		public void registerBeanDefinition(String name, BeanDefinition bd) {
-			this.beanMap.put(name, bd);
-		}
-	}
+	private final List<BeanFactoryPostProcessor> factoryPostProcessors = new LinkedList<>();
+	
 	
 	public AppContext(Class<?> configClass) {
 		this.reader = new BeanReader(this);
@@ -51,6 +57,12 @@ public class AppContext implements Registry {
 	
 	public void refresh() {
 		
+		prepareRefresh();
+		
+		// 调用工厂后置处理器
+		PostProcessorRegistrationDelegate.invokeBeanFactoryPostProcessors(factory);
+		
+		
 	}
 
 
@@ -59,5 +71,11 @@ public class AppContext implements Registry {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	private void prepareRefresh() {
+		this.datetime = LocalDateTime.now();
+		log.info("系统正在启动...");
+	}
+	
 	
 }
---
+++ src/com/analysis/context/BeanFactory.java
@@ -0,0 +1,26 @@
+package com.analysis.context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.analysis.bean.BeanDefinition;
+
+/**
+ * bean工厂类
+ * @author Cloud
+ *
+ */
+public class BeanFactory {
+
+	private Map<String, BeanDefinition> beanMap = new ConcurrentHashMap<>(256);
+	
+	private List<String> beanNames = new ArrayList<>(256);
+	
+	public void registerBeanDefinition(String name, BeanDefinition bd) {
+		this.beanMap.put(name, bd);
+		this.beanNames.add(name);
+	}
+	
+}
