f311f3697c645572c4e2b1ae4041ac63638c5513
ZhangHao <cloudzhanghi@foxmail.com>
2019-07-17 14:48:36 +0800

    实现解析Config注解包扫描功能

--- src/com/analysis/support/ConfigurationClassParser.java
+++ src/com/analysis/support/ConfigurationClassParser.java
@@ -1,15 +1,22 @@
 package com.analysis.support;
 
+import java.io.File;
 import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Logger;
 
 import com.analysis.annotation.Config;
 import com.analysis.bean.BeanDefinition;
 import com.analysis.bean.BeanDefinitionHolder;
 import com.analysis.context.BeanFactory;
 import com.analysis.metadata.AnnotationMetadata;
+import com.analysis.util.BeanNameGenerator;
 
 public class ConfigurationClassParser {
+	
+	private Logger log = Logger.getLogger(getClass().getName());
 
 	private BeanFactory factory;
 	
@@ -18,18 +25,62 @@ public class ConfigurationClassParser {
 	}
 	
 	public void parse(List<BeanDefinitionHolder> bdhs) {
+		
+		// 解析需要扫描的包
 		List<String> scanPackages = new ArrayList<>();
 		for (BeanDefinitionHolder bdh : bdhs) {
 			BeanDefinition bd = bdh.getBeanDefinition();
 			AnnotationMetadata am = bd.getAnnotationMetadata();
 			Annotation[] as = am.getAnnotations();
 			for (Annotation a : as) {
-				if (a.getClass().getSimpleName().equals("Config")) {
+				if (a.annotationType().getSimpleName().equals("Config")) {
 					Config config = (Config) a;
 					String[] packages = config.packages();
+					for (String pack : packages) {
+						scanPackages.add(pack);
+					}
 				}
 			}
 		}
+		
+		// 若包为空，则返回
+		if (scanPackages.size() == 0) {
+			return;
+		}
+		
+		// 根据包解析文件
+		List<String> javaFiles = new ArrayList<>();
+		String base = "./src/";
+		for (String pack : scanPackages) {
+			String processedPack = base + pack;
+			File f = new File(processedPack);
+			String[] ss = f.list((dir, filename) -> {
+				if (!filename.contains(".") && !filename.substring(filename.lastIndexOf(".")).equals(".java")) {
+					return false;
+				} else {
+					return true;
+				}
+			});
+			for (String s : ss) {
+				s = s.substring(0, s.indexOf("."));
+				String absoluteFile = pack + "/" + s;
+				absoluteFile = absoluteFile.replace("/", ".");
+				javaFiles.add(absoluteFile);
+			}
+		}
+		
+		// 把解析到的类注册到beanFactory中
+		for (String javaFile : javaFiles) {
+			try {
+				Class<?> clazz = Class.forName(javaFile);
+				BeanDefinition bd = new BeanDefinition(clazz);
+				String beanName = BeanNameGenerator.getName(clazz);
+				factory.registerBeanDefinition(beanName, bd);
+			} catch (ClassNotFoundException e) {
+				log.warning("类加载失败，未找到类" + javaFile);
+			}
+		}
+		
 	}
 	
 }
