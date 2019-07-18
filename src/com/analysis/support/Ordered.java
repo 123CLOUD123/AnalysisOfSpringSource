bda3d298b3869a5f3ba1bcb1af576bcac8eb00b6
ZhangHao <cloudzhanghi@foxmail.com>
2019-07-18 22:38:10 +0800

    添加接口

---
+++ src/com/analysis/support/Ordered.java
@@ -0,0 +1,10 @@
+package com.analysis.support;
+
+/**
+ * 用于确定执行顺序
+ * @author Cloud
+ *
+ */
+public interface Ordered {
+
+}
---
+++ src/com/analysis/support/PriorityOrdered.java
@@ -0,0 +1,10 @@
+package com.analysis.support;
+
+/**
+ * 用于设置优先级
+ * @author Cloud
+ *
+ */
+public interface PriorityOrdered {
+
+}
