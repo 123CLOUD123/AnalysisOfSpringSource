2b157fe645ae629882ee3550568b187e3aeea986
ZhangHao <cloudzhanghi@foxmail.com>
2019-07-20 22:28:06 +0800

    添加自定义异常

---
+++ src/com/analysis/exception/BeanException.java
@@ -0,0 +1,13 @@
+package com.analysis.exception;
+
+/**
+ * @author Cloud
+ * 与Bean有关的异常
+ *
+ */
+public class BeanException extends RuntimeException {
+
+	public BeanException(String msg) {
+		super(msg);
+	}
+}
